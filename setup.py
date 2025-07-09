938161294f96e8c3dadaa52cc6ff6ef0a8cc7981
Sanket Hanjage <sankethanjage1999@gmail.com>
2025-07-09 17:31:09 +0530

    Added setup.py with author info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Sanket Hanjage',  
+    author_email='sankethanjage1999@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn']
+)
